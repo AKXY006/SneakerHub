bef62b953775d163fc41c5fbeee4bbf97de468ad
AKXY006 <ak144336@gmail.com>
2026-06-27 01:16:39 +0530

    table created

--- src/main/java/com/sneakerhub/entity/User.java
+++ src/main/java/com/sneakerhub/entity/User.java
@@ -1,5 +1,70 @@
 package com.sneakerhub.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String firstname;
+	private String lastname;
+	private String email;
+	private String passward;
+	private String phone;
+	private String gender;
+	private Boolean active;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassward() {
+		return passward;
+	}
+	public void setPassward(String passward) {
+		this.passward = passward;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Boolean getActive() {
+		return active;
+	}
+	public void setActive(Boolean active) {
+		this.active = active;
+	}	
 
 }
